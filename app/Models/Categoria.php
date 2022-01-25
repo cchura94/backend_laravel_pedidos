32ad359a3efb5cdec1ab2a971d6ff79f5cf17306
cchura94 <cchura.cpc@gmail.com>
2022-01-24 20:57:58 -0400

    migrations y models

---
+++ app/Models/Categoria.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    //protected $table = 'mi_categoria';
+
+    //protected $primaryKey = 'idcat';
+    //protected $keyType = 'string';
+    //public $incrementing = false;
+
+    // public $timestamps = false;
+
+}
