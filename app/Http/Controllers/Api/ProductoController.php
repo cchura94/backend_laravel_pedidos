4b4a3e806e051c0a05dfb77c64158298031e4742
cchura94 <cchura.cpc@gmail.com>
2022-02-07 22:36:11 -0400

    Api crud productos

--- app/Http/Controllers/Api/ProductoController.php
+++ app/Http/Controllers/Api/ProductoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Producto;
 
 class ProductoController extends Controller
 {
@@ -14,7 +15,8 @@ class ProductoController extends Controller
      */
     public function index()
     {
-        //
+        $productos = Producto::paginate(10);
+        return response()->json($productos, 200);
     }
 
     /**
@@ -25,7 +27,29 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "nombre" => "required|min:3|max:200|unique:productos",
+            "precio" => "required",
+            "categoria_id" => "required"
+        ]);
+
+        $nom_img = "";
+        if($file = $request->file("imagen")){
+            $nom_img = time(). "-". $file->getClientOriginalName();
+            $file->move("imagenes");
+        }
+
+        $prod = new Producto();
+        $prod->nombre = $request->nombre;
+        $prod->precio = $request->precio;
+        $prod->stock = $request->stock;
+        $prod->descripcion = $request->descripcion;
+        $prod->estado = $request->estado;
+        $prod->categoria_id = $request->categoria_id;
+        $prod->imagen = '/imagenes' . $nom_img;  
+        $prod->save();   
+        
+        return response()->json(["mensaje" => "Producto registrado"], 201);
     }
 
     /**
@@ -36,7 +60,8 @@ class ProductoController extends Controller
      */
     public function show($id)
     {
-        //
+        $producto = Producto::find($id);
+        return response()->json($producto, 200);
     }
 
     /**
@@ -48,7 +73,28 @@ class ProductoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            "nombre" => "required|min:3|max:200|unique:productos,nombre,$id",
+            "precio" => "required",
+            "categoria_id" => "required"
+        ]);
+
+        $prod = Producto::find($id);
+        $prod->nombre = $request->nombre;
+        $prod->precio = $request->precio;
+        $prod->stock = $request->stock;
+        $prod->descripcion = $request->descripcion;
+        $prod->estado = $request->estado;
+        $prod->categoria_id = $request->categoria_id;
+        $nom_img = "";
+        if($file = $request->file("imagen")){
+            $nom_img = time(). "-". $file->getClientOriginalName();
+            $file->move("imagenes");
+            $prod->imagen = '/imagenes' . $nom_img;     
+        }
+        $prod->save();
+        
+        return response()->json(["mensaje" => "Producto Modificado"], 200);
     }
 
     /**
@@ -59,6 +105,9 @@ class ProductoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $prod = Producto::find($id);
+        $prod->delete();
+
+        return response()->json(["mensaje" => "Producto Eliminado"], 200);
     }
 }
